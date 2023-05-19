a9bf45891a1d607b36244ff3b7355e714f675380
Ozcan YILDIZHAN <ozcan.14785@gmail.com>
2023-05-19 09:42:20 +0300

    add get,set method
    Class yapısının içine get ve set metodlarını ekledim.

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ class Magaza:
         self.__magaza_adi = magaza_adi
         self.__satislar = {}
         self.ekle_satis(satici_adi, satici_cinsi, 0)
+
+    def get_magaza_adi(self):
+        return self.__magaza_adi
+
+    def set_magaza_adi(self, magaza_adi):
+        self.__magaza_adi = magaza_adi
